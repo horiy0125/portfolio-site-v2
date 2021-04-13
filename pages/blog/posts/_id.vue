2bd30b79350ee6f931a0586b5dfd2869f7d7924f
horri1520 <kh1520.shsf@gmail.com>
2021-04-13 09:12:51 +0900

    fix: show not-found-page on dynamic routing

--- pages/blog/posts/_id.vue
+++ pages/blog/posts/_id.vue
@@ -1,6 +1,8 @@
 <template>
   <ps-seperate-view :browsed-page-path="pagePath">
+    <div v-if="invalidPostId">404 Not found.</div>
     <ps-blog-post-detail
+      v-else
       :page-url="pageUrl"
       :post-detail="blogPostDetailViewData"
     />
@@ -16,25 +18,49 @@ import apiRequestHeaders from '~/config/api/api-request-headers';
 import domain from '~/config/domain';
 import pagePaths from '~/config/page-paths';
 import FetchPostApiResponse from '~/types/config/api/fetch-post';
+import FetchPostsApiResponse from '~/types/config/api/fetch-posts';
 import blogPostDetailViewModel from '~/view-models/blog-post-detail';
 export default Vue.extend({
   components: { PsSeperateView, PsBlogPostDetail },
-  async asyncData({ $axios, params }) {
-    const id = params.id;
-    const url = `${apiEndpoints.posts}/${id}`;
 
-    const res: FetchPostApiResponse = await $axios.$get(url, {
-      headers: apiRequestHeaders,
+  async asyncData({ $axios, params }) {
+    const postsRes: FetchPostsApiResponse = await $axios.$get(
+      apiEndpoints.posts,
+      {
+        headers: apiRequestHeaders,
+      },
+    );
+
+    const validPostIds: string[] = [];
+
+    postsRes.contents.forEach(post => {
+      validPostIds.push(post.id);
     });
 
-    const blogPostDetailViewData = blogPostDetailViewModel(res);
+    const id = params.id;
 
-    const pagePath = `${pagePaths.blogPost}/${id}`;
+    if (validPostIds.includes(id)) {
+      const postRes: FetchPostApiResponse = await $axios.$get(
+        `${apiEndpoints.posts}/${id}`,
+        {
+          headers: apiRequestHeaders,
+        },
+      );
 
+      const blogPostDetailViewData = blogPostDetailViewModel(postRes);
+
+      const pagePath = `${pagePaths.blogPost}/${id}`;
+
+      return {
+        pageUrl: domain + pagePath,
+        pagePath,
+        blogPostDetailViewData,
+        invalidPostId: false,
+      };
+    }
     return {
-      pageUrl: domain + pagePath,
-      pagePath,
-      blogPostDetailViewData,
+      invalidPostId: true,
+      pagePath: pagePaths.notFound,
     };
   },
 });
